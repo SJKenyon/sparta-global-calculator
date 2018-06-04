3641cba97b9ba2e7d281a9c896f1637925e4ea3a
SJKenyon <skenyon@spartaglobal.com>
2018-06-04 14:58:46 +0100

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# sparta-global-calculator
+# Calculator
+
+## Functionality
+
+### Normal
+
+Basic:
+- Addition
+- Multiplication
+- Division
+- Subtraction
+- Modulus
+
+Advanced:
+- Power
+- Square root
+
+### BMI Calculator
+The user is able to choose between the Metric and Imperial system at the start for ease of use. It then gets them to input their height and weight to calculate their BMI.
+
+
+### Trip Calculator
+This feature asks the user for four inputs; distance, fuel efficiency (mpg), cost per gallon, speed. It then calculates the time and price of the journey. However, for every 1 MPH over 60 MPH, the MPG is reduced by 2.
+
+---
+
+### Cloning
+
+This file system does not need any additional files downloading.
--- calculator.js
+++ calculator.js
@@ -1,44 +1,43 @@
-var k = 1;
+var k = true;
 
-while (k < 9999999) {
+while (k == true) {
 
-// get the user inputs
-var x = parseInt(prompt('Enter first value'));
-var y = parseInt(prompt('Enter the second value'));
-var choice = prompt("Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide (mo)dulus (p)ower (sq)uare root");
+  // get the user inputs
+  var choice = prompt("Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide (mo)dulus (p)ower (sq)uare root");
+  var x = parseInt(prompt('Enter first value'));
+  var y = parseInt(prompt('Enter the second value'));
 
-if(choice == "a") {
-  var a = (x + y);
-  console.log(a);
-}
-else if(choice == "s") {
-  var s = (x - y);
-  console.log(s);
-}
-else if(choice == "m") {
-  var m = (x * y);
-  console.log(m);
-}
-else if(choice == "d") {
-  var d = (x / y);
-  console.log(d);
-}
-else if(choice == "mo") {
-  var mo = (x % y);
-  console.log(mo);
-}
-else if(choice == "p") {
-  var p = (x ** y);
-  console.log(p);
-}
-else if(choice == "sq") {
-  console.log("Square root of first number: " + Math.sqrt(x));
-  console.log("Square root of first number: " + Math.sqrt(y));
-}
-else {
-  console.log("ERROR")
-}
+  if(choice == "a") {
+    var a = (x + y);
+    console.log(a);
+  }
+  else if(choice == "s") {
+    var s = (x - y);
+    console.log(s);
+  }
+  else if(choice == "m") {
+    var m = (x * y);
+    console.log(m);
+  }
+  else if(choice == "d") {
+    var d = (x / y);
+    console.log(d);
+  }
+  else if(choice == "mo") {
+    var mo = (x % y);
+    console.log(mo);
+  }
+  else if(choice == "p") {
+    var p = (x ** y);
+    console.log(p);
+  }
+  else if(choice == "sq") {
+    console.log("Square root of first number: " + Math.sqrt(x));
+    console.log("Square root of second number: " + Math.sqrt(y));
+  }
+  else {
+    console.log("ERROR");
+  }
 
-console.log("Answer number " + k);
-k++;
+  k = prompt("Exit? (true)/(false)");
 }
